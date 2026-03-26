cbaab7f7f55c867e71b1abd19ff3b3d1f7cee4ab
Amber-Mr-chen <astrotarot@dev.com>
2026-03-26 21:43:44 +0800

    fix: Add NEXT_PUBLIC_PAYPAL_CLIENT_ID to next.config.js env

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEXT_PUBLIC_PAYPAL_CLIENT_ID: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = config.externals || []
