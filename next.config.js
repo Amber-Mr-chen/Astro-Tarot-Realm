754949dc06766aeec26a8c1585900bff8ffc2a4f
root <root@localhost.localdomain>
2026-03-26 12:33:34 +0800

    fix: Add crypto to webpack externals for Cloudflare Workers compatibility

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || []
+      config.externals.push('crypto')
+    }
+    return config
+  }
+}
 module.exports = nextConfig
